a05cd777a67af14f4e40638c04494b51b18a7b9d
Billy <billy@dreammx.com>
2015-12-02 10:01:41 +0800

    update sudo

--- ubuntu-14.04-setup.sh
+++ ubuntu-14.04-setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y update && sudo apt-get -y upgrade
-sudo apt-get -y install build-essential subversion git httpie proxychains
-sudo apt-get -y install tmux screen proxychains indicator-multiload hamster-indicator indicator-china-weather ibus-googlepinyin ibus-table-wubi
-sudo apt-get -y install p7zip-full rar unrar chromium-browser filezilla
+sudo -u root apt-get -y update && sudo -u root apt-get -y upgrade
+sudo -u root apt-get -y install build-essential subversion git httpie proxychains
+sudo -u root apt-get -y install tmux screen proxychains indicator-multiload hamster-indicator indicator-china-weather ibus-googlepinyin ibus-table-wubi
+sudo -u root apt-get -y install p7zip-full rar unrar chromium-browser filezilla
 
 # setup vim
-sudo apt-get -y install vim-gnome
+sudo -u root apt-get -y install vim-gnome
 git clone https://github.com/billychow/.vim.git
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -19,9 +19,9 @@ rm -rf ~/.vim/fonts
 cd ~/Downloads
 wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
 tar xf Sublime\ Text\ 2.0.2\ x64.tar.bz2
-sudo mv Sublime\ Text\ 2 /opt/
-sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/local/bin/sublime
-sudo cat <<EOD > /usr/share/applications/sublime.desktop
+sudo -u root mv Sublime\ Text\ 2 /opt/
+sudo -u root ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/local/bin/sublime
+sudo -u root cat <<EOD > /usr/share/applications/sublime.desktop
 [Desktop Entry]
 Version=1.0
 Name=Sublime Text 2
@@ -40,10 +40,10 @@ Exec=sublime -n
 TargetEnvironment=Unity
 EOD
 # set sublime as default text-editor in stead of gedit
-sudo sed -i "s/gedit.desktop/sublime.desktop/g" /usr/share/applications/defaults.list
+sudo -u root sed -i "s/gedit.desktop/sublime.desktop/g" /usr/share/applications/defaults.list
 
 # development tools
-sudo apt-get -y install memcached mongodb redis-server imagemagick openjdk-7-jdk
+sudo -u root apt-get -y install memcached mongodb redis-server imagemagick openjdk-7-jdk
 
 # setup pyenv & python
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
@@ -52,7 +52,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 exec $SHELL
-sudo apt-get -y install libreadline-dev libbz2-dev libsqlite3-dev
+sudo -u root apt-get -y install libreadline-dev libbz2-dev libsqlite3-dev
 pyenv install 2.7.10
 pyenv global 2.7.10
 pyenv rehash
@@ -67,7 +67,7 @@ echo -e '\n' >> ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 exec $SHELL
-sudo apt-get install -y libreadline-dev
+sudo -u root apt-get install -y libreadline-dev
 rbenv install 2.2.3
 rbenv global 2.2.3
 rbenv rehash
@@ -75,5 +75,5 @@ rbenv rehash
 gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
 
 # setup nodejs & nvm
-sudo apt-get -y install npm
+sudo -u root apt-get -y install npm
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
