e695270ad9e8898f72c4bee9ca8840a6846647b0
Billy <billy@dreammx.com>
2015-12-02 18:02:27 +0800

    update centos-7.setup.sh

--- centos-7.setup.sh
+++ centos-7.setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# to disable selinux
 yum -y update
 yum -y groupinstall 'Development Tools'
-yum -y install tmux screen subversion git
+yum -y install epel-release
+yum -y install tmux screen subversion git \
+               mariadb-server nginx
+
+systemctl enable mariadb-server
+systemctl start mariadb-server
+
+systemctl enable nginx
+systemctl start nginx
