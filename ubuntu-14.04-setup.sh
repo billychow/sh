b579fce5530c9fe1734ab155ac5f666370bdd909
Billy <billy@dreammx.com>
2015-12-12 00:38:37 +0800

    add zsh

--- ubuntu-14.04-setup.sh
+++ ubuntu-14.04-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo -u root apt-get -y update && sudo -u root apt-get -y upgrade
-sudo -u root apt-get -y install build-essential subversion git vim-gnome \
+sudo -u root apt-get -y install build-essential subversion git vim-gnome zsh \
                                 tmux screen proxychains indicator-multiload hamster-indicator indicator-china-weather \
                                 ibus-googlepinyin ibus-table-wubi \
                                 p7zip-full rar unrar chromium-browser filezilla \
