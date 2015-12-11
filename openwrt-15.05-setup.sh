89a78a079fccd561f06a3768aa5a9131c307cb44
Billy <billy@dreammx.com>
2015-12-12 00:47:39 +0800

    update openwrt.sh

--- openwrt-15.05-setup.sh
+++ openwrt-15.05-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 opkg update
-opkg install luci luci-ssl ddns-scripts luci-app-ddns luci-app-vnstat luci-app-upnp luci-app-wol msmtp screen
-opkg install shadowsocks-libev-spec-polarssl
+opkg install luci luci-ssl ddns-scripts luci-app-ddns \
+             luci-app-vnstat luci-app-upnp luci-app-wol \
+             luci-app-hd-idle \
+             screen vim-full git patch wget curl hd-idle msmtp rsync
+opkg install shadowsocks-libev-polarssl
