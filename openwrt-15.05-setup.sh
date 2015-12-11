2683aef2a129f010fd83afc4130b40d59f6eafd5
Billy <billy@dreammx.com>
2015-12-12 01:28:12 +0800

    update openwrt.sh

--- openwrt-15.05-setup.sh
+++ openwrt-15.05-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# set timezone
+uci set system.@system[0].zonename='Asia/Shanghai'
+uci set system.@system[0].timezone='CST-8'
+
 opkg update
 opkg install luci luci-ssl ddns-scripts luci-app-ddns \
              luci-app-vnstat luci-app-upnp luci-app-wol \
              luci-app-hd-idle \
              screen vim-full git patch wget curl hd-idle msmtp rsync
 opkg install shadowsocks-libev-polarssl
+
+mkdir ~/download
+mkdir -p /data/logs/vnstat
