67d0f2c967bef923b9702c79dc9dc3b921bd5094
Billy <billy@dreammx.com>
2015-12-15 18:01:08 +0800

    update

--- centos-7-setup.sh
+++ centos-7-setup.sh
@@ -2,9 +2,15 @@
 
 # to disable selinux
 yum -y update
+
+mkdir -p ~/download && cd ~/download
+wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+rpm -Uvh remi-release-7.rpm
 yum -y groupinstall 'Development Tools'
 yum -y install epel-release
 yum -y install tmux screen subversion git \
+               php-cli php-devel php-fpm php-mbstring php-mysqlnd \
+               php-apc php-memcache php-redis \
                mariadb-server nginx
 
 systemctl enable mariadb-server
