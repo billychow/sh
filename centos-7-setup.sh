d46d8f3343fbf8dc901186f4956d5775eab0bf27
Billy <billy@dreammx.com>
2015-12-16 00:09:52 +0800

    update centos7.sh

--- centos-7-setup.sh
+++ centos-7-setup.sh
@@ -8,13 +8,20 @@ wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 rpm -Uvh remi-release-7.rpm
 yum -y groupinstall 'Development Tools'
 yum -y install epel-release
-yum -y install tmux screen subversion git \
+yum -y install tmux screen subversion git bash-completion \
                php-cli php-devel php-fpm php-mbstring php-mysqlnd \
                php-apc php-memcache php-redis \
-               mariadb-server nginx
+               mariadb-server ruby pygpgme curl
 
-systemctl enable mariadb-server
-systemctl start mariadb-server
+gem install bundler --no-rdoc --no-ri
+curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
+yum -y install nginx passenger
+
+systemctl enable mariadb.service
+systemctl start mariadb.service
+
+systemctl enable php-fpm.service
+systemctl start php-fpm.service
 
 systemctl enable nginx
 systemctl start nginx
