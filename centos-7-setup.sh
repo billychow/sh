6711e79f60346f4e22e6098628c6e42565432984
Billy <billy@dreammx.com>
2015-12-16 16:36:24 +0800

    update

--- centos-7-setup.sh
+++ centos-7-setup.sh
@@ -8,14 +8,20 @@ wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 rpm -Uvh remi-release-7.rpm
 yum -y groupinstall 'Development Tools'
 yum -y install epel-release
-yum -y install tmux screen subversion git bash-completion fail2ban \
+yum -y install tmux screen subversion git bash-completion fail2ban iptables-services \
                php-cli php-devel php-fpm php-mbstring php-mysqlnd \
-               php-apc php-memcache php-redis \
+               php-apc php-memcache php-redis php-gd php-mcrypt \
                mariadb-server ruby pygpgme curl
 
 gem install bundler --no-rdoc --no-ri
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
 yum -y install nginx passenger
+
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+
+systemctl enable iptables.service
+systemctl start iptables.service
 
 systemctl enable fail2ban.service
 systemctl start fail2ban.service
