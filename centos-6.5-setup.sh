b96887c46c74f34a8a969970273b0e173974ccaa
Billy <billy@dreammx.com>
2015-12-24 23:23:34 +0800

    add jenkins

--- centos-6.5-setup.sh
+++ centos-6.5-setup.sh
@@ -6,9 +6,10 @@ yum -y groupinstall 'Development Tools'
 yum -y install epel-release
 # Add phusion passenger el6 YUM repository
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo && rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum -y install tmux screen subversion git \
                php-cli php-devel php-fpm php-mbstring php-mysqlnd
-               mysql-server mysql-devel nginx passenger \
+               mysql-server mysql-devel nginx passenger jenkins \
                ruby ruby-devel rubygems pygpgme curl \
                ImageMagick-devel java-1.8.0-openjdk postfix mailx alpine
 
--- centos-7-setup.sh
+++ centos-7-setup.sh
@@ -8,11 +8,12 @@ wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 rpm -Uvh remi-release-7.rpm
 yum -y groupinstall 'Development Tools'
 yum -y install epel-release
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo && rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum -y install tmux screen subversion git bash-completion fail2ban iptables-services \
                php-cli php-devel php-fpm php-mbstring php-mysqlnd \
                php-apc php-memcache php-redis php-gd php-mcrypt \
                mariadb-server mariadb-devel ruby ruby-devel ImageMagick-devel java-1.8.0-openjdk \
-               pygpgme curl zlib-devel openssl-devel postfix mailx alpine
+               pygpgme curl zlib-devel openssl-devel postfix mailx alpine jenkins
 
 gem install bundler --no-rdoc --no-ri
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
