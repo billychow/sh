fc0db9737c17b565098e27eb9ab894e99ce2809b
Billy <billy@dreammx.com>
2015-12-25 00:59:09 +0800

    add diffutils

--- openwrt-15.05-setup.sh
+++ openwrt-15.05-setup.sh
@@ -9,8 +9,12 @@ opkg update
 opkg install luci luci-ssl luci-app-ddns \
              luci-app-vnstat luci-app-upnp luci-app-wol luci-app-qos \
              luci-app-hd-idle luci-app-privoxy luci-app-transmission \
-             screen vim-full git patch wget curl hd-idle msmtp rsync
+             screen vim-full git patch wget curl hd-idle msmtp rsync \
+             diffutils
 opkg install shadowsocks-libev-polarssl
 
 mkdir ~/download
 mkdir -p /data/logs/vnstat
+
+# apply patches
+patch -p1 -d /usr/lib/lua/luci/view/admin_status/ < ~/target/openwrt/patches/add_hostname_to_associated_station.patch
