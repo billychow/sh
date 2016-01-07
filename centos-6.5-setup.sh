8a692057605ba389514c4bf26df3c01135b64d6e
Billy <billy@dreammx.com>
2016-01-07 21:37:47 +0800

    add pecl

--- centos-6.5-setup.sh
+++ centos-6.5-setup.sh
@@ -8,7 +8,8 @@ yum -y install epel-release
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo && rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum -y install tmux screen subversion git bind-utils \
-               php-cli php-devel php-fpm php-mbstring php-mysqlnd
+               php-cli php-devel php-fpm php-mbstring php-mysqlnd \
+               php-apc php-memcache php-redis php-gd php-mcrypt php-soap \
                mysql-server mysql-devel nginx passenger jenkins \
                ruby ruby-devel rubygems pygpgme curl \
                ImageMagick-devel java-1.8.0-openjdk postfix mailx alpine
