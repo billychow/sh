d37757a045d52560275e781b54bea56ec9c61aff
Billy <billy@dreammx.com>
2016-01-16 16:53:05 +0800

    update

--- ubuntu-14.04-setup.sh
+++ ubuntu-14.04-setup.sh
@@ -13,7 +13,7 @@ git clone https://github.com/billychow/.vim.git
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # install powerline fonts
-git clone https://github.com/powerline/fonts.git ~/.vim/fonts
+#git clone https://github.com/powerline/fonts.git ~/.vim/fonts
 ~/.vim/fonts/install.sh
 rm -rf ~/.vim/fonts
 
@@ -22,7 +22,7 @@ cd ~/Downloads
 wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
 tar xf Sublime\ Text\ 2.0.2\ x64.tar.bz2
 sudo -u root mv Sublime\ Text\ 2 /opt/
-sudo -u root ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/local/bin/sublime
+sudo -u root ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/local/bin/subl
 sudo -u root cat <<EOD > /usr/share/applications/sublime.desktop
 [Desktop Entry]
 Version=1.0
@@ -38,11 +38,24 @@ X-Ayatana-Desktop-Shortcuts=NewWindow
 
 [NewWindow Shortcut Group]
 Name=New Window
-Exec=sublime -n
+Exec=subl -n
 TargetEnvironment=Unity
 EOD
 # set sublime as default text-editor in stead of gedit
 sudo -u root sed -i "s/gedit.desktop/sublime.desktop/g" /usr/share/applications/defaults.list
+
+# setup rbenv & ruby-build
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo -e '\n' >> ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+rbenv install 2.2.3
+rbenv global 2.2.3
+rbenv rehash
+# setup mirror for gem source
+gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
 
 # setup pyenv & python
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
@@ -58,18 +71,15 @@ pyenv rehash
 pip install tornado==2.1.1
 pip install sae-python-dev
 
-# setup rbenv & ruby-build
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo -e '\n' >> ~/.bashrc
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
-rbenv install 2.2.3
-rbenv global 2.2.3
-rbenv rehash
-# setup mirror for gem source
-gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
-
 # setup nvm
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# set system proxy
+pip install gfwlist2pac
+pyenv rehash
+wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
+gfwlist2pac -i gfwlist.txt -f proxy.pac -p "SOCKS5 127.0.0.1:1080;"
+sudo -u root mv proxy.pac /etc/proxy.pac
+rm -f gfwlist.txt
+dconf write /system/proxy/mode "'auto'"
+dconf write /system/proxy/autoconfig-url "'file:///etc/proxy.pac'"
