cf6192fa2488da7e58d115353380b290ddb249b2
Billy <billy@dreammx.com>
2016-09-20 16:08:08 +0800

    update

--- openwrt-15.05-setup.sh
+++ openwrt-15.05-setup.sh
@@ -5,20 +5,31 @@ uci set system.@system[0].zonename='Asia/Shanghai'
 uci set system.@system[0].timezone='CST-8'
 uci commit
 
+mkdir ~/download
+
+echo "src/gz openwrt_dist http://openwrt-dist.sourceforge.net/releases/ar71xx/packages" >> /etc/opkg.conf
+echo "src/gz openwrt_dist_luci http://openwrt-dist.sourceforge.net/releases/luci/packages" >> /etc/opkg.conf
+
 opkg update
 opkg install luci luci-ssl luci-app-ddns \
-             luci-app-vnstat luci-app-upnp luci-app-wol luci-app-qos \
-             luci-app-hd-idle luci-app-privoxy luci-app-transmission \
-             screen vim-full git-http patch wget curl hd-idle msmtp rsync \
+             luci-app-upnp luci-app-wol luci-app-qos \
+             #luci-app-redsocks2 \
+             #luci-app-privoxy \
+             luci-app-transmission transmission-web \
+             screen vim-full git-http patch wget curl msmtp rsync \
              diffutils \
+             openssh-client \
+             openssh-sftp-server \
              kmod-usb-core kmod-usb2 kmod-usb3 kmod-usb-storage kmod-usb-storage-extras \
              block-mount kmod-fs-ext4 kmod-fs-exfat kmod-fs-vfat luci-app-samba \
+             hd-idle \
+             luci-app-hd-idle \
              kmod-nf-nathelper-extra \
-             luci-app-chinadns luci-app-shadowsocks shadowsocks-libev-polarssl
-opkg install shadowsocks-libev-polarssl
+             shadowsocks-libev-polarssl \
+             luci-app-shadowsocks \
+             luci-app-chinadns
 
-mkdir ~/download
-mkdir -p /data/logs/vnstat
+opkg install luci-app-vnstat && mkdir -p /data/logs/vnstat
 
 # apply patches
-patch -p1 -d /usr/lib/lua/luci/view/admin_status/ < ~/target/openwrt/patches/add_hostname_to_associated_station.patch
+patch -p1 -d /usr/lib/lua/luci/view/admin_status/ < ../target/openwrt/patches/add_hostname_to_associated_station.patch
